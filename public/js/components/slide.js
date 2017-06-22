20217eb2543df67a4b4a2b19bb14df52326123bd
Naomi Villanueva <naovillaj@gmail.com>
2017-06-22 16:53:17 -0500

    esquema1

--- public/js/components/slide.js
+++ public/js/components/slide.js
@@ -9,20 +9,20 @@ const Slide = () => {
   slide_container.append(slide);
   slide_container.append(Title("Paga a tus amigos"));
   slide_container.append(Instructions("Paga a quien quieras desde donde quieras, sin usar efectivo"));
-  slide_container.append(Slide_buttons());
+  // slide_container.append(Slide_buttons());
 
   return slide_container;
 }
 
-const Slide_buttons = () => {
-  const slide_buttons = $("<div class='slide-buttons text-center'></div>");
-  const btn_1 = $("<span class='btn-slide bg-redBullet'></span>");
-  const btn_2 = $("<span class='btn-slide bg-gray'></span>");
-  const btn_3 = $("<span class='btn-slide bg-gray'></span>");
+// const Slide_buttons = () => {
+//   const slide_buttons = $("<div class='slide-buttons text-center'></div>");
+//   const btn_1 = $("<span class='btn-slide bg-redBullet'></span>");
+//   const btn_2 = $("<span class='btn-slide bg-gray'></span>");
+//   const btn_3 = $("<span class='btn-slide bg-gray'></span>");
 
-  slide_buttons.append(btn_1);
-  slide_buttons.append(btn_2);
-  slide_buttons.append(btn_3);
+//   slide_buttons.append(btn_1);
+//   slide_buttons.append(btn_2);
+//   slide_buttons.append(btn_3);
 
-  return slide_buttons;
-}
+//   return slide_buttons;
+// }
